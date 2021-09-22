a497c6f715941c103dc4bf9500351584bef14beb
Saar Hen <saarhe@post.bgu.ac.il>
2021-09-22 13:17:22 +0300

    Ex6

--- MyFirstServer.js
+++ MyFirstServer.js
@@ -1,6 +1,29 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('Hello World!');
-    res.end();
-}).listen(8080);
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const sql = require("./db.js");
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to web course example application." });
+});
+
+app.get("/Saar", function(req, res){
+    sql.query("select * from saar", (err, mysqlres) => {
+        if (err) {
+            console.log("error: ", err);
+            res.status(400).send({message: "error in getting info: " + err});
+            return;
+        }
+        console.log("got all the info..");
+        res.send(mysqlres);
+        return;
+    });
+});
+
+app.listen(3000, () => {
+console.log("Server is running on port 3000.");
+});
---
+++ db config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "pLadaxd1",
+    DB: "mysql"
+};
